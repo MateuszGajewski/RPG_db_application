911ecdfc32dbd1e5ec2f25878db299d35a4d3bd5
Eryk Kosmala <73111059+kosmpolita@users.noreply.github.com>
2020-12-18 16:03:14 +0100

    wesja 2.8.3 - select, update, insert character

--- main.py
+++ main.py
@@ -8,11 +8,14 @@ class window():
     def __init__(self):
         self.database = None
         self.lista = []
+        self.canLoad = 1
         self.createdInt = 1
         self.tabOfAb = 1
+        self.createdPostacie = 1
         self.lista_class = ["---------"]
         self.lista_sex = ["N", "F", "M"]
         self.lista_stat = []
+        self.postacie_index = []
         for i in range(21):
             self.lista_stat.append(i)
         self.lista_tab = ["Karty Postaci", "Klasy & Umiejętności", "Rasy & Pochodzenie", "Zarządzanie danymi"]
@@ -23,6 +26,7 @@ class window():
                 tmp += " "
                 n -= 1
             self.lista_tab[t] = self.lista_tab[t] + tmp
+        self.lista_postaci = ["---------"]
 
 
     def createDB(self):
@@ -55,7 +59,7 @@ class window():
             app.destroySubWindow("Logowanie")
 
     def sendCharacter(self):
-        nazwa = app.getEntry("Nazwa Postaci")
+        nazwa = app.getEntry("Nazwa")
         levela = app.getOptionBox("Level Postaci")
         plec  = app.getOptionBox("Płeć")
         rasa  = app.getEntry("Rasa")
@@ -78,7 +82,9 @@ class window():
         self.database.cursor().execute(f"insert into Postacie values (POSTACIE_SEQ.NEXTVAL,'{nazwa}', {levela}, {wiek}, {wzrost}, {waga}, '{plec}', '{skora}',"
                                        f"'{wlosy}', '{oczy}', {hp}, {sta}, {strg}, {dex}, {con}, {inte}, {wis}, {cha}, '{rasa}', '{pochodzenia}', '{klasa}')")
         self.database.cursor().execute("commit")
-        app.infoBox("Nowa Postać", "Nastąpiło poprawne dodanie postaci do bazy danych", parent=None)
+        app.infoBox("Nowa Postać   ", "Nastąpiło poprawne dodanie postaci do bazy danych", parent=None)
+        self.lista_postaci.append(nazwa)
+        app.changeOptionBox("Lista postaci : ", self.lista_postaci)
 
     def doNothing(self):
         pass
@@ -113,11 +119,26 @@ class window():
         self.lista = []
         self.createdInt = 1
         self.lista_class = ["---------"]
+        self.createdPostacie = 1
+        self.lista_postaci = ["---------"]
+        self.canLoad = 1
         app.openTab("Start", self.lista_tab[1])
         app.changeOptionBox("Wybierz klasę", self.lista_class, 0, 0)
         app.changeOptionBox("Klasa", self.lista_class, 0, 0)
+        app.changeOptionBox("Klasa :", self.lista_class, 0, 0)
         app.stopTab()
         app.infoBox("Wylogowywanie", "Nastąpiło poprawne wylogowanie z Bazy Danych", parent=None)
+
+    def createTableOfCharacters(self):
+        if self.database is not None and self.createdPostacie == 1:
+            self.lista_postaci = []
+            self.postacie_index = []
+            self.createdPostacie = 0
+            postacieDB = self.database.cursor().execute('select * from postacie')
+            for row in postacieDB:
+                self.lista_postaci.append(row[1])
+                self.postacie_index.append([row[0], row[1]])
+            app.changeOptionBox("Lista postaci : ", self.lista_postaci)
 
     def createTableOfClass(self):
         if self.database is not None and self.createdInt == 1:
@@ -138,7 +159,7 @@ class window():
                     tmp.append(item)
                 klasyLista.append(tmp)
             app.openTab("Start", self.lista_tab[0])
-            app.changeOptionBox("Klasa", self.lista_class, 1, 0)
+            app.changeOptionBox("Klasa :", self.lista_class, 1, 0)
             app.stopTab()
             app.openTab("Start", self.lista_tab[1])
             app.changeOptionBox("Wybierz klasę", self.lista_class , 1 , 0)
@@ -147,6 +168,78 @@ class window():
             app.setPadding([20, 20])
             app.addTable("tabela_klas", klasyLista, wrap="700", colspan=3)
             app.stopTab()
+
+    def loadCharacterData(self):
+        if self.database is not None and self.canLoad == 1:
+            character = app.getOptionBox("Lista postaci : ")
+            postacDB = self.database.cursor().execute(f"select * from postacie where nazwa_postaci = '{character}'")
+            for row in postacDB:
+                postacDB = row
+            app.setEntry("Nazwa :", postacDB[1])
+            app.setOptionBox("Level Postaci :", postacDB[2])
+            app.setOptionBox("Płeć :", postacDB[6])
+
+            app.setEntry("Rasa :", postacDB[18])
+            app.setEntry("Pochodzenie :", postacDB[19])
+            app.setOptionBox("Klasa :", postacDB[20])
+
+            app.setEntry("Wiek :", postacDB[3])
+            app.setEntry("Wzrost :", postacDB[4])
+            app.setEntry("Waga :", postacDB[5])
+
+            app.setEntry("Kolor skóry :", postacDB[7])
+            app.setEntry("Kolor włosów :", postacDB[8])
+            app.setEntry("Kolor oczów :", postacDB[9])
+
+            app.setSticky("e")
+            app.setEntry("HP :", postacDB[10])
+            app.setSticky("w")
+            app.setEntry("STA :", postacDB[11])
+            app.setSticky("w")
+            # app.addLabelEntry("Plik z avatarem", 7, 1, colspan=2)
+
+            app.setSticky("e")
+            app.setOptionBox("STR :", postacDB[12])
+            app.setOptionBox("DEX :", postacDB[13])
+            app.setOptionBox("CON :", postacDB[14])
+            app.setOptionBox("INT :", postacDB[15])
+            app.setOptionBox("WIS :", postacDB[16])
+            app.setOptionBox("CHA :", postacDB[17])
+
+
+    def editCharachterData(self):
+        postac = app.getOptionBox("Lista postaci : ")
+        nazwa = app.getEntry("Nazwa :")
+        levela = app.getOptionBox("Level Postaci :")
+        plec = app.getOptionBox("Płeć :")
+        rasa = app.getEntry("Rasa :")
+        pochodzenia = app.getEntry("Pochodzenie :")
+        klasa = app.getOptionBox("Klasa :")
+        wiek = app.getEntry("Wiek :")
+        wzrost = app.getEntry("Wzrost :")
+        waga = app.getEntry("Waga :")
+        skora = app.getEntry("Kolor skóry :")
+        wlosy = app.getEntry("Kolor włosów :")
+        oczy = app.getEntry("Kolor oczów :")
+        hp = app.getEntry("HP :")
+        sta = app.getEntry("STA :")
+        strg = app.getOptionBox("STR :")
+        dex = app.getOptionBox("DEX :")
+        con = app.getOptionBox("CON :")
+        inte = app.getOptionBox("INT :")
+        wis = app.getOptionBox("WIS :")
+        cha = app.getOptionBox("CHA :")
+        self.database.cursor().execute(
+            f"update postacie set nazwa_postaci = '{nazwa}', poziom = {levela}, plec = '{plec}', nazwa_rasy = '{rasa}', "
+            f"nazwa_kraju = '{pochodzenia}', nazwa_klasy= '{klasa}', wiek = {wiek}, wzrost = {wzrost}, waga = {waga}, "
+            f"kolor_skory = '{skora}', kolor_włosow ='{wlosy}', kolor_oczow = '{oczy}', hp = {hp}, sta = {sta},  "
+            f"str = {strg}, dex = {dex}, con = {con}, int = {inte}, wis = {wis}, cha = {cha} where nazwa_postaci = '{postac}'")
+        for pse in range(len(self.lista_postaci)):
+            if self.lista_postaci[pse] == postac:
+                self.lista_postaci[pse] = nazwa
+                app.changeOptionBox("Lista postaci : ", self.lista_postaci)
+        self.database.cursor().execute("commit")
+        app.infoBox("Zmień dane", "Dane wskazanej postaci zostały poprawnie zmienione", parent=None)
 
     def createTableOfAbilities(self):
         if self.tabOfAb > 1:
@@ -184,12 +277,70 @@ class window():
                 app.setPadding([20, 20])
                 app.addTable(f"tabela_umiejetnosci_dla_klas{self.tabOfAb}", umiejLista, wrap="500")
 
+    def createNewCharacterWindow(self):
+            with app.subWindow("Dodaj nową postać", modal = True):
+                app.showSubWindow("Dodaj nową postać")
+                app.setResizable(canResize=False)
+                app.setSize(1280, 720)
+                app.setBg(colour="beige", override=False)
+                app.setStretch("column")
+                app.setSticky("new")
+                app.addImage("smokPostac", "dnd2.gif", 0, colspan=4)
+
+                app.setPadding([20, 0])
+                app.setStretch("column")
+                app.setSticky("w")
+                app.addLabel("KartaPos", "Karta Postaci", 1, 0)
+                app.addButtons(["Zatwierdź postać"], win.sendCharacter, 1, 1)
+
+                app.setSticky("w")
+                app.addLabelEntry("Nazwa", 2, 0)
+                app.addLabelOptionBox("Level Postaci", win.lista_stat, 2, 1)
+                app.addLabelOptionBox("Płeć", win.lista_sex, 2, 2)
+
+                app.addLabelEntry("Rasa", 3, 0)
+                app.addLabelEntry("Pochodzenie", 3, 1)
+                app.addLabelOptionBox("Klasa", win.lista_class, 3, 2)
+
+                app.addLabelEntry("Wiek", 4, 0)
+                app.addLabelEntry("Wzrost", 4, 1)
+                app.addLabelEntry("Waga", 4, 2)
+
+                app.addLabelEntry("Kolor skóry", 5, 0)
+                app.addLabelEntry("Kolor włosów", 5, 1)
+                app.addLabelEntry("Kolor oczów", 5, 2)
+
+                app.setSticky("e")
+                app.addLabelEntry("HP", 6, 0)
+                app.setSticky("w")
+                app.addLabelEntry("STA", 6, 1)
+                app.setSticky("w")
+                # app.addLabelEntry("Plik z avatarem", 7, 1, colspan=2)
+
+                app.setSticky("e")
+                app.addLabelOptionBox("STR", win.lista_stat, 7, 0)
+                app.addLabelOptionBox("DEX", win.lista_stat, 7, 1)
+                app.addLabelOptionBox("CON", win.lista_stat, 8, 0)
+                app.addLabelOptionBox("INT", win.lista_stat, 8, 1)
+                app.addLabelOptionBox("WIS", win.lista_stat, 9, 0)
+                app.addLabelOptionBox("CHA", win.lista_stat, 9, 1)
+
+                app.setPadding([0, 0])
+                app.setSticky("nwes")
+                app.addImage("krol", "krol.gif", 1, 3, rowspan=50)
+                # app.addImage("avatar", "avatar.gif", 8, 1)
+
+                # app.setImageSize("avatar", width=300, height=300)
+                # app.setImage("avatar", "avatar.gif")
+
+
+
 if __name__ == "__main__" :
 
     win = window()
     with gui('Baza Danych RPG') as app:
         app.setResizable(canResize=True)
-        app.setSize(1280, 880)
+        app.setSize(1280, 720)
         fileMenu = ["Login", "Log Out"]
         app.addMenuList("Open", fileMenu, [win.makeLogin, win.makeLogOut])
         app.disableMenuItem("Open", "Log Out")
@@ -200,48 +351,58 @@ if __name__ == "__main__" :
         app.setStretch("column")
         app.setSticky("new")
         app.addImage("tlo1", "dnd2.gif", 0, colspan=4)
-
+        app.setSticky("we")
         app.setPadding([20, 0])
+        app.addLabelOptionBox("Lista postaci : ", win.lista_postaci, 1, 0, colspan=2)
+        app.addLabel("pustaGlowna1", "", 1, 2)
+        app.setSticky("w")
+        app.addButtons(["Edytuj Postać"], win.loadCharacterData, 2, 0)
+        app.addButtons(["Nowa Postać   "], win.createNewCharacterWindow, 2, 1)
+
+        
         app.setStretch("column")
         app.setSticky("w")
-        app.addLabel("KartaPos", "Karta Postaci", 1, 0)
-        app.addButtons(["Zatwierdź postać"], win.sendCharacter, 1, 1)
+        app.addLabel("KartaPos_g", "Karta Postaci", 3, 0)
+        app.addButtons(["Zmień dane postać"], win.doNothing, 3, 1)
 
         app.setSticky("w")
-        app.addLabelEntry("Nazwa Postaci", 2, 0)
-        app.addLabelOptionBox("Level Postaci", win.lista_stat, 2, 1)
-        app.addLabelOptionBox("Płeć", win.lista_sex, 2, 2)
+        app.addLabelEntry("Nazwa :", 4, 0)
+        app.addLabelOptionBox("Level Postaci :", win.lista_stat, 4, 1)
+        app.addLabelOptionBox("Płeć :", win.lista_sex, 4, 2)
 
-        app.addLabelEntry("Rasa", 3, 0)
-        app.addLabelEntry("Pochodzenie", 3, 1)
-        app.addLabelOptionBox("Klasa", win.lista_class, 3, 2)
+        app.addLabelEntry("Rasa :", 5, 0)
+        app.addLabelEntry("Pochodzenie :", 5, 1)
+        app.addLabelOptionBox("Klasa :", win.lista_class, 5, 2)
 
-        app.addLabelEntry("Wiek", 4, 0)
-        app.addLabelEntry("Wzrost", 4, 1)
-        app.addLabelEntry("Waga", 4, 2)
+        app.addLabelEntry("Wiek :", 6, 0)
+        app.addLabelEntry("Wzrost :", 6, 1)
+        app.addLabelEntry("Waga :", 6, 2)
 
-        app.addLabelEntry("Kolor skóry", 5, 0)
-        app.addLabelEntry("Kolor włosów", 5, 1)
-        app.addLabelEntry("Kolor oczów", 5, 2)
+        app.addLabelEntry("Kolor skóry :", 7, 0)
+        app.addLabelEntry("Kolor włosów :", 7, 1)
+        app.addLabelEntry("Kolor oczów :", 7, 2)
 
         app.setSticky("e")
-        app.addLabelEntry("HP", 6, 0)
+        app.addLabelEntry("HP :", 8, 0)
         app.setSticky("w")
-        app.addLabelEntry("STA", 6, 1)
+        app.addLabelEntry("STA :", 8, 1)
         app.setSticky("w")
         #app.addLabelEntry("Plik z avatarem", 7, 1, colspan=2)
 
-        app.setSticky("w")
-        app.addLabelOptionBox("STR", win.lista_stat, 7, 0)
-        app.addLabelOptionBox("DEX", win.lista_stat, 8, 0)
-        app.addLabelOptionBox("CON", win.lista_stat, 9, 0)
-        app.addLabelOptionBox("INT", win.lista_stat, 10, 0)
-        app.addLabelOptionBox("WIS", win.lista_stat, 11, 0)
-        app.addLabelOptionBox("CHA", win.lista_stat, 12, 0)
+        app.setSticky("e")
+        app.addLabelOptionBox("STR :", win.lista_stat, 9, 0)
+        app.addLabelOptionBox("DEX :", win.lista_stat, 9, 1)
+        app.addLabelOptionBox("CON :", win.lista_stat, 10, 0)
+        app.addLabelOptionBox("INT :", win.lista_stat, 10, 1)
+        app.addLabelOptionBox("WIS :", win.lista_stat, 11, 0)
+        app.addLabelOptionBox("CHA :", win.lista_stat, 11, 1)
+        app.setSticky("we")
+        app.addButtons(['Zmień dane'], win.editCharachterData, 11, 2)
 
         app.setPadding([0,0])
-        app.setSticky("nwes")
-        app.addImage("krol", "krol.gif", 1, 3, rowspan=50)
+        app.setSticky("nes")
+        app.addImage("krolGlowny", "krol.gif", 1, 3, rowspan=50)
+        app.registerEvent(win.createTableOfCharacters)
         #app.addImage("avatar", "avatar.gif", 8, 1)
 
         #app.setImageSize("avatar", width=300, height=300)
